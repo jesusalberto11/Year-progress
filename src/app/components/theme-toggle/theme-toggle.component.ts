b07f6c015952984b3d677cd84ff7162a3a1bf5ca
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-04-17 21:35:51 -0500

    Feat: Implemented theme change logic

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.css']
+  styleUrls: ['./theme-toggle.component.css'],
 })
-export class ThemeToggleComponent {
+export class ThemeToggleComponent implements OnInit {
+  currentTheme: string = 'light-theme';
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    this.checkForCurrentTheme();
+  }
+
+  checkForCurrentTheme(): void {
+    const theme: string = localStorage.getItem('theme')!;
+    this.currentTheme = theme;
+  }
+
+  changeTheme(): void {
+    this.checkForCurrentTheme();
+
+    this.document.body.classList.replace(
+      this.currentTheme,
+      this.currentTheme === 'light-theme'
+        ? (this.currentTheme = 'dark-theme')
+        : (this.currentTheme = 'light-theme')
+    );
+
+    localStorage.setItem('theme', this.currentTheme);
+  }
 }
