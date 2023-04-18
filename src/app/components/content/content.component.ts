4bc867f61e009dde69200e29a85c67e6fb340693
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-04-18 14:54:09 -0500

    Feat: Now current month updates on language change

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-content',
@@ -9,41 +10,57 @@ export class ContentComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
   currentDay: number = new Date().getDate();
   currentMonth: string = '';
+  actualDate = new Date();
+  currentDayPercentaje: number = 0;
+  remainingDays: number = 0;
+  totalMilisecondsInADay: number = 1000 * 60 * 60 * 24;
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.updateLanguage(event.lang);
+    });
+  }
 
   ngOnInit(): void {
-    let currentLanguage = localStorage.getItem('app_languaje')?.toString();
-    this.currentMonth = new Date().toLocaleString(currentLanguage, {
+    let currentLanguage = localStorage.getItem('app_language')?.toString();
+    this.updateLanguage(currentLanguage);
+    this.calculateCurrentDayPercentaje();
+  }
+
+  updateLanguage(language?: string): void {
+    this.currentMonth = new Date().toLocaleString(language, {
       month: 'long',
     });
   }
 
-  actualDate = new Date();
+  calculateCurrentDayPercentaje(): void {
+    let currentDayInMiliseconds = this.actualDate.getTime();
+    let firstDayOfTheYearInMiliseconds = new Date(
+      this.actualDate.getFullYear(),
+      0,
+      1
+    ).getTime();
 
-  currentDayInMiliseconds = this.actualDate.getTime();
-  firstDayOfTheYearInMiliseconds = new Date(
-    this.actualDate.getFullYear(),
-    0,
-    1
-  ).getTime();
+    let numberOfTheCurrentDay =
+      (currentDayInMiliseconds - firstDayOfTheYearInMiliseconds) /
+        this.totalMilisecondsInADay +
+      1;
 
-  //1000 miliseconds in a second, 60 seconds in a minute, 60 minutes in a hour and 24 hours in a day
-  totalMilisecondsInADay = 1000 * 60 * 60 * 24;
+    this.currentDayPercentaje = Math.round((numberOfTheCurrentDay / 365) * 100);
 
-  numberOfTheCurrentDay =
-    (this.currentDayInMiliseconds - this.firstDayOfTheYearInMiliseconds) /
-      this.totalMilisecondsInADay +
-    1;
+    this.calculateRemainingDays(currentDayInMiliseconds);
+  }
 
-  currentDayPercentaje = Math.round((this.numberOfTheCurrentDay / 365) * 100);
+  calculateRemainingDays(currentDayInMiliseconds: number): void {
+    let lastDayOfTheYearInMiliseconds = new Date(
+      this.actualDate.getFullYear(),
+      11,
+      31
+    ).getTime();
 
-  lastDayOfTheYearInMiliseconds = new Date(
-    this.actualDate.getFullYear(),
-    11,
-    31
-  ).getTime();
-
-  remainingDays = Math.ceil(
-    (this.lastDayOfTheYearInMiliseconds - this.currentDayInMiliseconds) /
-      this.totalMilisecondsInADay
-  );
+    this.remainingDays = Math.ceil(
+      (lastDayOfTheYearInMiliseconds - currentDayInMiliseconds) /
+        this.totalMilisecondsInADay
+    );
+  }
 }
