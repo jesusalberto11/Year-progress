5a110a9c42e7efcba78351a846eb1e2eb17e5817
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-04-18 17:09:18 -0500

    Feat: Added system to display messages in different languages

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { interval } from 'rxjs';
 import { MESSAGES } from '../data/messages-data';
 
@@ -10,12 +11,36 @@ import { MESSAGES } from '../data/messages-data';
 export class MessagesComponent implements OnInit {
   currentMessage: string =
     'Aprovecha bien tu tiempo, ya que cuando quieras ver, se habrá ido volando';
+  currentLanguage = 'es';
+  currentIndex: number = -1;
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLanguage = event.lang;
+      this.updateCurrentMessage();
+    });
+  }
 
   ngOnInit() {
     const intervalo = interval(10000);
+    this.currentLanguage = localStorage.getItem('app_language')!;
+    this.updateCurrentMessage();
 
-    intervalo.subscribe((timer) => {
-      this.currentMessage = MESSAGES[timer % MESSAGES.length];
+    intervalo.subscribe(() => {
+      this.updateCurrentMessage();
     });
+  }
+
+  updateCurrentMessage(): void {
+    const messageArray = MESSAGES[this.currentLanguage];
+
+    let newIndex = Math.floor(Math.random() * messageArray.length);
+
+    while (newIndex === this.currentIndex) {
+      newIndex = Math.floor(Math.random() * messageArray.length);
+    }
+
+    this.currentIndex = newIndex;
+    this.currentMessage = messageArray[this.currentIndex];
   }
 }
